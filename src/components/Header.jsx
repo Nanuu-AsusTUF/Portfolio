4fb331a2c33cdef6c0fba4c1014ce681461b0eda
Nanuuu <ngurahdanuhputra@gmail.com>
2025-05-17 13:35:33 +0800

    update progress

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,31 @@
 const Header = () => {
-    return (
-      <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
-  <div className="max-w-screen-2xl w-full mx-auto px-4">
-    <h1>
-      <a href="/" className="logo">
-        <img src="/images/porto-logo.png" alt="logo navbar" width={40} height={40}/>
-      </a>
-    </h1>
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
+      <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr] ">
+        <h1>
+          <a href="/" className="logo">
+            <img
+              src="/images/porto-logo.png"
+              alt="logo navbar"
+              width={40}
+              height={40}
+            />
+          </a>
+        </h1>
 
-    <div className="">
-  <button className="" onClick={null}>
-    <span className="material-symbols-rounded">menu</span>
-  </button>
-  Navbar
-</div>
+        <div className="relative md:justify-self-center">
+          <button className="menu-btn md:hidden" onClick={null}>
+            <span className="material-symbols-rounded">menu</span>
+          </button>
+          Navbar
+        </div>
 
-<a href="#" className="">Contact Me</a>
-
-  </div>
-</header>
-    )
+        <a href="#" className="">
+          Contact Me
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
