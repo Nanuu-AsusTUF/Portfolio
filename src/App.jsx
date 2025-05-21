7474312c9d252226ee78ffc2d29236a0a654cfa2
Nanuuu <ngurahdanuhputra@gmail.com>
2025-05-21 18:51:30 +0800

    NEW UPDATE

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/about";
+import Skill from "./components/skill";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <main>
         <Hero />
         <About />
+        <Skill />
       </main>
     </>
   );
