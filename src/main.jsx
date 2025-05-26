0fd80ce48b66d7bf086f187f749ee26bbdb77a99
Nanuuu <ngurahdanuhputra@gmail.com>
2025-05-26 22:04:38 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx"; // Ensure App is correctly imported
-import "./index.css"; // Import TailwindCSS and custom styles
+import App from "./App.jsx";
+import "./index.css";
 import "lenis/dist/lenis.css";
 
 createRoot(document.getElementById("root")).render(
