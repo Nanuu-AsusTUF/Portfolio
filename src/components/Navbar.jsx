74557e685929c810ec0e5291b4ba32abd19b0321
Nanuuu <ngurahdanuhputra@gmail.com>
2025-05-29 00:48:33 +0800

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = ({ navOpen }) => {
       className: "nav-link",
     },
     {
-      label: "Work",
+      label: "Project",
       link: "#work",
       className: "nav-link",
     },
